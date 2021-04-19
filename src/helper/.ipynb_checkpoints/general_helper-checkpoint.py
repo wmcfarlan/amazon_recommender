237e32d2a3907a2b0d1bb1c89468928cbf371799
walker mcfarlan <walkeremcfarlan@gmail.com>
2021-04-19 15:40:06 -0700

    added ml

--- src/helper/.ipynb_checkpoints/general_helper-checkpoint.py
+++ src/helper/.ipynb_checkpoints/general_helper-checkpoint.py
@@ -1,5 +1,8 @@
 import gzip
 import pandas as pd
+import itertools
+import numpy as np
+from lightfm import LightFM
 
 def parse(path):
     """
@@ -37,6 +40,43 @@ def review_map(row):
     else:
         return 'low'
     
+    
+def feature_colon_value(my_list):
+    """
+    Takes as input a list and prepends the columns names to respective values in the list.
+    For example: if my_list = [1,1,0,'del'],
+    resultant output = ['f1:1', 'f2:1', 'f3:0', 'loc:del']
+   
+    """
+    result = []
+    ll = ['free:','prod_review_counts:', 'categories:']
+    aa = my_list
+    for x, y in zip(ll, aa):
+        res = str(x) +""+ str(y)
+        result.append(res)
+    return result
+
+
+
+
+def sample_hyperparameters():
+    """
+    Yield possible hyperparameter choices.
+    """
+
+    while True:
+        yield {
+            "no_components": np.random.randint(16, 100),
+            "learning_schedule": np.random.choice(["adagrad", "adadelta"]),
+            "loss": np.random.choice(["bpr", "warp", "warp-kos"]),
+            "learning_rate": np.random.exponential(0.05),
+            "item_alpha": np.random.exponential(1e-8),
+            "max_sampled": np.random.randint(5, 15),
+            "num_epochs": np.random.randint(5, 100),
+        }
+        
+
+
 if __name__ == '__main__':
     print('main')
     
