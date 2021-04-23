10b334bc936fe219e6bfe07acfd658990ae1035b
walker mcfarlan <walkeremcfarlan@gmail.com>
2021-04-22 17:37:30 -0700

    added keras modeling

--- src/.ipynb_checkpoints/custom_recommender-checkpoint.py
+++ src/.ipynb_checkpoints/custom_recommender-checkpoint.py
@@ -7,6 +7,9 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
 
 class custom_predictor:
 
@@ -21,6 +24,15 @@ class custom_predictor:
 #     def get_target_col(self):
 #         self.target_column = self.dataframe.groupby('asin').count().sort_values(
 #             by='reviewerID', ascending=False)['reviewerID'].index[0]
+
+#     def fill_asin_title_dict(self):
+        
+#         asin_title = self.dataframe[['asin', 'title']]
+#         asin_title.drop_duplicates()['title']
+#         asin_title.index = asin_title.asin
+        
+#         self.asin_title_dict = dict(asin_title['title'])
+#         self.title_asin_dict = {v:k for k, v in self.asin_title_dict.items()}
     
     def set_index(self):
         
@@ -103,6 +115,7 @@ class custom_predictor:
         vectorizer = TfidfVectorizer(stop_words='english')
         vect = vectorizer.fit_transform(self.nlp_df.text_data)
         
+        #testing np
         self.vect_df = pd.DataFrame(data=vect.todense(), index=self.nlp_df.index, columns=vectorizer.get_feature_names())
 
             
@@ -113,12 +126,15 @@ class custom_predictor:
         fit the model to generate proper data for prediction
         """
         
+#         self.fill_asin_title_dict()
         self.set_index()
         self.populate_dictionary(text_columns)
         self.get_targets()
         self.create_nlp_df()
     
-    def predict(self, model):
+    
+    
+    def predict_one_eff(self, model, product):
         
         """
         Creates predictions for each item in pivot_df space to fill out missing data
@@ -135,7 +151,81 @@ class custom_predictor:
         
         """
         
+        
         model = model
+        pred_dict = defaultdict(int)
+        
+        full_set = self.vect_df.merge(self.pivot_matrix.loc[:, product], left_index=True, right_index=True)
+        full_set.fillna(0, inplace=True)
+        full_set = full_set.to_numpy()
+        
+        filled_data = full_set[full_set[:, -1] != 0]
+        
+        X = filled_data[:, :-1]
+        y = filled_data[:, -1]
+        X_train, X_test, y_train, y_test = train_test_split(X, y)
+        
+        model.fit(X_train, y_train)
+        preds_test = model.predict(X_test)
+        
+        error = mean_squared_error(y_test, preds_test)
+        
+        print(error)
+        
+        true_preds = model.predict(full_set[:, :-1])
+        pred_dict[product] = true_preds
+        
+        return pred_dict
+    
+    
+    
+    def predict_one(self, model, product):
+        
+        """
+        Creates predictions for each item in pivot_df space to fill out missing data
+        
+        
+        Arguments
+        ---------
+        model: sklearn regressor, the parameters can be user selected
+        
+        
+        Returns
+        ---------
+        Returns a dictionary with the name of each product and predictions for each user
+        
+        """
+        
+        
+        model = model
+        pred_dict = defaultdict(int)
+        
+        full_set = self.vect_df.merge(self.pivot_matrix.loc[:, product], left_index=True, right_index=True)
+        full_set.fillna(0, inplace=True)
+        filled_data = full_set[full_set[product] != 0]
+        
+        X = filled_data.iloc[:, :-1]
+        y = filled_data.iloc[:, -1]
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y)
+        
+        model.fit(X_train, y_train)
+        preds_test = model.predict(X_test)
+        
+        error = mean_squared_error(y_test, preds_test)
+        
+        print(error)
+        
+        true_preds = model.predict(full_set.iloc[:, :-1])
+        pred_dict[product] = true_preds
+        
+        return pred_dict
+    
+    
+    def predict_all(self, model):
+        
+        model = model
+        error_dict = defaultdict(int)
         pred_dict = defaultdict(int)
         for col in self.target_list:
             full_set = self.vect_df.merge(self.pivot_matrix.loc[:, col], left_index=True, right_index=True)
@@ -144,13 +234,31 @@ class custom_predictor:
             
             X = filled_data.iloc[:, :-1]
             y = filled_data.iloc[:, -1]
-            model.fit(X, y)
+            
+            X_train, X_test, y_train, y_test = train_test_split(X, y)
+            
+            
+            model.fit(X_train, y_train)
+            preds_test = model.predict(X_test)
+            
+            error = mean_squared_error(y_test, preds_test)
+            error_dict[col] = error
             
             preds = model.predict(full_set.iloc[:, :-1])
             pred_dict[col] = preds
-        return pred_dict
+        return pred_dict, error_dict
             
+
+        
+        
+        
+class cluster_rec:
     
+    def __init__ (self):
+        pass
+        
+        
+        
 if __name__ == "__main__":
     print('main')
         
---
+++ src/helper/.ipynb_checkpoints/keras_class-checkpoint.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from zipfile import ZipFile
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+
+class RecommenderNet(keras.Model):
+    def __init__(self, num_users, num_products, embedding_size, **kwargs):
+        super(RecommenderNet, self).__init__(**kwargs)
+        self.num_users = num_users
+        self.num_movies = num_products
+        self.embedding_size = embedding_size
+        self.user_embedding = layers.Embedding(
+            num_users,
+            embedding_size,
+            embeddings_initializer="he_normal",
+            embeddings_regularizer=keras.regularizers.l2(1e-6),
+        )
+        self.user_bias = layers.Embedding(num_users, 1)
+        self.product_embedding = layers.Embedding(
+            num_products,
+            embedding_size,
+            embeddings_initializer="he_normal",
+            embeddings_regularizer=keras.regularizers.l2(1e-6),
+        )
+        self.product_bias = layers.Embedding(num_products, 1)
+
+    def call(self, inputs):
+        user_vector = self.user_embedding(inputs[:, 0])
+        user_bias = self.user_bias(inputs[:, 0])
+        product_vector = self.product_embedding(inputs[:, 1])
+        product_bias = self.product_bias(inputs[:, 1])
+        dot_user_product = tf.tensordot(user_vector, product_vector, 2)
+        # Add all the components (including bias)
+        x = dot_user_product + user_bias + product_bias
+        # The sigmoid activation forces the rating to between 0 and 1
+        return tf.nn.sigmoid(x)
+    
+if __name__ == "__main__":
+    print('main')
+        
